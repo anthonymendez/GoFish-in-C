c92f0763ac7fc371f98a094aec7192e76bf64db1
Anthony Mendez <anthonymendez9@gmail.com>
2018-10-08 14:20:45 -0400

    Filled in Get Random Rank in Hand and changed Rank to a single char instead of a 2D Array

--- card.h
+++ card.h
@@ -2,13 +2,13 @@
 #define CARD_H
 
 /*
-  Valid suits: C, D, H, and S
-  Valid ranks: 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A
-*/
+ * Valid suits: C, D, H, and S
+ * Valid ranks: 2, 3, 4, 5, 6, 7, 8, 9, T, J, Q, K, A
+ */
 struct card
 {
   char suit;
-  char rank[2];
+  char rank;
 };
 
 /*
