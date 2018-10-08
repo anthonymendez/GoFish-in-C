23d6dab9e93f44c4b92fc8283d467d94b4f3e75f
Anthony Mendez <anthonymendez9@gmail.com>
2018-10-08 15:08:10 -0400

    Created quickTest Shell Script to compile and test

---
+++ quickTest.sh
@@ -0,0 +1,8 @@
+echo "----------------------------"
+echo "Compiling GoFish Program..."
+make
+echo "----------------------------"
+echo "Compilation Process Completed... Running..."
+./gofish
+echo "----------------------------"
+echo "Done!"
